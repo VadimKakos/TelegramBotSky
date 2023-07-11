a866743d8411fc11630ff57d43a7bcfe35ef01d6
vadimkosolapov <vilgutis@icloud.com>
2023-07-11 20:18:50 +0500

    добавил listen

--- src/main/java/com/example/telegrambotsky/listener/TelegramBotListener.java
+++ src/main/java/com/example/telegrambotsky/listener/TelegramBotListener.java
@@ -1,4 +1,15 @@
 package com.example.telegrambotsky.listener;
 
-public class TelegramBotListener {
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TelegramBotListener implements UpdatesListener {
+    @Override
+    public int process(List<Update> list) {
+        return 0;
+    }
 }
